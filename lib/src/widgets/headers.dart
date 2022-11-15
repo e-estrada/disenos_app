588e976e9ed2697f385db062ef3aabddf5ead4a4
Erick Estrada <eof105@gmail.com>
2022-11-15 10:15:23 -0600

    Header Waves

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -187,3 +187,45 @@ class _HeaderCurvoPainter extends CustomPainter {
     return true;
   }
 }
+
+
+class HeaderWaves extends StatelessWidget {
+  const HeaderWaves({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavesPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    // Propiedades
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = Path();
+    // Dibujar con el path y el paint
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3, size.width * .5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.2, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
