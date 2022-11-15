8ff12161eadf5a456bf7fc84dee1618a835f453a
Erick Estrada <eof105@gmail.com>
2022-11-15 09:38:54 -0600

    CustomPainter

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -26,3 +26,44 @@ class HeaderBordesRedondeados extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    // Propiedades
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+    // Dibujar con el path y el paint
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * .30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
