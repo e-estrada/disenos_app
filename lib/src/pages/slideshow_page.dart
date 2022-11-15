5089a8adab69a9abd97870602ecc187aaa6b57b3
Erick Estrada <eof105@gmail.com>
2022-11-15 17:54:36 -0600

    Diseños de los puntos indicadores

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -8,8 +8,45 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _Slides(),
+        child: Column(
+          children: [
+            Expanded(
+              child: _Slides()
+            ),
+            _Dots()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
     );
   }
 }
@@ -19,10 +56,10 @@ class _Slides extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: PageView(
-        children: [
-          const _Slide('assets/svgs/slide-1.svg'),
-          const _Slide('assets/svgs/slide-2.svg'),
-          const _Slide('assets/svgs/slide-3.svg'),
+        children: const [
+          _Slide('assets/svgs/slide-1.svg'),
+          _Slide('assets/svgs/slide-2.svg'),
+          _Slide('assets/svgs/slide-3.svg'),
         ],
       ),
     );
