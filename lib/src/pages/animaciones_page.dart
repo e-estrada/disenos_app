0958001d27852f3b6111b03170ca614b6886d043
Erick Estrada <eof105@gmail.com>
2022-11-15 11:19:17 -0600

    Animation & AnimationController

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class AnimacionesPage extends StatelessWidget {
   const AnimacionesPage({super.key});
 
@@ -19,23 +20,41 @@ class CuadradoAnimado extends StatefulWidget {
   State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 4000));
+
+    rotacion = Tween(
+      begin: 0.0,
+      end: 2.0
+    ). animate(controller);
+    
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return _Rectangulo();
   }
 }
 
-
 class _Rectangulo extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-       width: 70,
-       height: 70,
-       decoration: const BoxDecoration(
-         color: Colors.blue
-       ),
-     );
-   }
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(color: Colors.blue),
+    );
+  }
 }
