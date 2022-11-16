04722ccfa5d582d021cb2395564c179f7f1ad07a
Erick Estrada <eof105@gmail.com>
2022-11-16 17:44:03 -0600

    Parametrizando IconHeader

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:disenos_app/src/widgets/headers.dart';
+import 'package:disenos_app/src/widgets/icon_header.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 class EmergencyPage extends StatelessWidget {
@@ -8,7 +9,13 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: IconHeader(),
+      body: IconHeader(
+        titulo: 'Asistencia Médica',
+        subTitulo: 'Haz solicitado',
+        icon: FontAwesomeIcons.plus,
+        color1: Color(0xff526BF6),
+        color2: Color(0xff67ACF2),
+      ),
    );
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuadrado extends StatelessWidget {
   const HeaderCuadrado({super.key});
@@ -189,6 +188,7 @@ class _HeaderCurvoPainter extends CustomPainter {
   }
 }
 
+
 class HeaderWaves extends StatelessWidget {
   const HeaderWaves({super.key});
 
@@ -229,6 +229,7 @@ class _HeaderWavesPainter extends CustomPainter {
     return true;
   }
 }
+
 
 class HeaderWaveGradient extends StatelessWidget {
   const HeaderWaveGradient({super.key});
@@ -280,86 +281,5 @@ class _HeaderWaveGradientPainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
-  }
-}
-
-class IconHeader extends StatelessWidget {
-  const IconHeader({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color colorBlanco = Colors.white.withOpacity(0.7);
-    return Stack(
-      children: [
-        
-        
-        const _IconHeaderBackground(),
-
-
-        Positioned(
-            top: -50,
-            left: -70,
-            child: FaIcon(
-              FontAwesomeIcons.plus,
-              size: 250,
-              color: colorBlanco,
-            )),
-        
-        Column(
-          children: [
-            const SizedBox(height: 80, width: double.infinity),
-
-            Text(
-              'Haz solicitado',
-              style: TextStyle(fontSize: 20, color: colorBlanco),
-            ),
-
-
-            const SizedBox(height: 20),
-
-
-            Text(
-              'Asistencia',
-              style: TextStyle(fontSize: 25, color: colorBlanco, fontWeight: FontWeight.bold),
-            ),
-
-            const SizedBox(height: 20),
-
-            const FaIcon(
-              FontAwesomeIcons.plus,
-              size: 80,
-              color: Colors.white,
-            ),
-
-          ],
-        )
-      ],
-    );
-  }
-}
-
-class _IconHeaderBackground extends StatelessWidget {
-  const _IconHeaderBackground({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 300,
-      decoration: const BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xff526BF6),
-            Color(0xff67ACF2),
-          ],
-        ),
-      ),
-    );
   }
 }
